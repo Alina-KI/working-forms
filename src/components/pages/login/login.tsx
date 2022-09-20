2b9da45010e5f21cb32d9e115981688d8e93e307
Alina-KI <kraynova.alina.2002@mail.ru>
2022-09-20 20:37:37 +0300

    created login form

--- src/components/pages/login/login.tsx
+++ src/components/pages/login/login.tsx
@@ -3,8 +3,25 @@ import s from './login.module.scss'
 
 export const Login = () => {
   return (
-    <div>
-
+    <div className={s.box}>
+      <form className={s.form}>
+        <h2 className={s.title}>Sign in</h2>
+        <div className={s.blockInput}>
+          <input className={s.input} type="text" required={true}/>
+          <span className={s.label}>Username</span>
+          <i className={s.i}> </i>
+        </div>
+        <div className={s.blockInput}>
+          <input className={s.input} type="password" required={true}/>
+          <span className={s.label}>Password</span>
+          <i className={s.i}> </i>
+        </div>
+        <div className={s.blockLink}>
+          <span className={s.link}>Forgot password</span>
+          <span className={s.link}>Sing up</span>
+        </div>
+        <button className={s.login}>Login</button>
+      </form>
     </div>
   )
 }
