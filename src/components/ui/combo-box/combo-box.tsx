3522606eee78c3c5c4b47f617bd4c4e02ac806e9
Alina-KI <kraynova.alina.2002@mail.ru>
2022-09-22 12:17:13 +0300

    fixed combo-box

--- src/components/ui/combo-box/combo-box.tsx
+++ src/components/ui/combo-box/combo-box.tsx
@@ -1,25 +1,37 @@
 import React, { useMemo, useState } from 'react'
 import s from './combo-box.module.scss'
+import { useInternalClick } from '../../../hooks/use-internal-click'
 
 type Props = {
   title: string
   data: string[]
+  value: string
+  setValue: (isOpen: string) => void
 }
 
-export const ComboBox = ({ title, data }: Props) => {
-  const [isOpen, setIsOpen] = useState(false)
-  const [value, setValue] = useState('')
+export const ComboBox = ({ title, data, value, setValue }: Props) => {
   const newData = useMemo(() => {
     return data.filter((item: string) => item.includes(value))
   }, [value])
+  const [isOpen, setIsOpen] = useState(false)
+  useInternalClick(s.comboBox, {
+      onExternalClick: () => setIsOpen(false),
+      onInternalClick: () => setIsOpen(isOpen => !isOpen),
+    }
+  )
 
   return (
     <div className={s.comboBox}>
-      <div className={s.blockInput} onClick={() => setIsOpen(!isOpen)}>
-        <input value={value} onChange={(e) => setValue(e.target.value)} className={s.input} type="text" required={true}/>
+      <div className={s.blockInput}>
+        <input value={value} onChange={(e) => setValue(e.target.value)} className={s.input} type="text"
+               required={true}/>
         <span className={s.label}>{title}</span>
         <i className={s.i}> </i>
-        <svg className={`${ s.icon } ${isOpen && `${s.activeIcon}`}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg>
+        <svg className={`${s.icon} ${isOpen && `${s.activeIcon}`}`} xmlns="http://www.w3.org/2000/svg"
+             viewBox="0 0 512 512">
+          <path
+            d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/>
+        </svg>
       </div>
       {isOpen &&
       <div className={s.data}>
